5e7acdf41d4e7ded8694f2c1f927e54bc82a1c66
Rajani Kanth BATHULA <rajanib.in@mouritech.com>
2021-06-16 02:46:01 +0530

    aded allure reports

--- src/test/java/com/accelerator/automation/runners/DefaultRunner.java
+++ src/test/java/com/accelerator/automation/runners/DefaultRunner.java
@@ -7,7 +7,11 @@ import io.cucumber.testng.CucumberOptions;
 		glue = {"com.accelerator.automation.stepdefs.ecom","com.accelerator.automation.hooks"},
 		plugin = {"pretty",
 				"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:",
-				"timeline:target/test-output-thread/"},
+				"timeline:target/test-output-thread/",
+				"json:target/cucumber-report/cucumber.json",
+				"io.qameta.allure.cucumber5jvm.AllureCucumber5Jvm"
+
+		},
 		features = {"src/test/resources/features/ECOM/"}
 	)
 
